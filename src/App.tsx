256e9c6bde774f9d4b8e9de0210eb6c88375046e
XiangZhuang <xzhuangaa@connect.ust.hk>
2022-01-24 20:23:11 +0800

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./components/UI/Header/Header";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Header />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
